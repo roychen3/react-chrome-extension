8ea15a20e096e24c644365f3957a2f772d744406
Roy Chen <roy850323@gmail.com>
2022-10-24 16:25:30 +0800

    temp

--- webpack/common.js
+++ webpack/common.js
@@ -1,8 +1,14 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+  entry: {
+    popup: './src/popup/index.js',
+    background: './src/background/index.js',
+    content: './src/content/index.js',
+  },
   module: {
     rules: [
       {
@@ -26,14 +32,24 @@ module.exports = {
     ],
   },
   plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'src/manifest.json',
+          to: path.join(__dirname, '../build'),
+        },
+      ],
+    }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
+      filename: 'popup.html',
+      chunks: ['popup'],
+      cache: false,
     }),
     new ESLintPlugin({
       extensions: ['js', 'ts'],
     }),
   ],
-  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, '../build'),
     filename: '[name].bundle.js',
